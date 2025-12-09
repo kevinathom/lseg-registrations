af63df0de65b902f77e4ceb5c27a4ae42cd3da54
kevinathom <44673990+kevinathom@users.noreply.github.com>
2025-12-09 17:56:02 -0500

    Create process.py
    Started non-code notes for process flow

---
+++ code/process.py
@@ -0,0 +1,13 @@
+# Open files
+#ProductRegistrationSummaryRequest_YYYYMMDD.csv
+#AccountstoCheck-LSEG.xlsx
+
+# Remove old records from today's file
+#Full duplicate compared to master file
+
+# Flag potential duplicates
+#Full duplicates in today's file
+#Partial duplicates in today's file
+#Partial duplicate compared to master file
+
+# 

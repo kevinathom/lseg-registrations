bafae266529bb240175437756005716bf0c9f145
kevinathom <44673990+kevinathom@users.noreply.github.com>
2026-01-15 17:19:09 -0500

    Update process.py
    Revise added field names for clarity

--- code/process.py
+++ code/process.py
@@ -23,27 +23,27 @@ dat_today.drop(dat_today[pd.merge(dat_today, dat_ongoing, on=list(dat_today.colu
 dat_today.reset_index(inplace=True, drop=True)
 
 # Flag potential issues
-dat_today["Email_Prefix"] = dat_today["COMPANY EMAIL"].str.extract(r'(.+?(?=\@))')
-dat_today["Flag_Incoming"] = ""
+dat_today["Email_local-part"] = dat_today["COMPANY EMAIL"].str.extract(r'(.+?(?=\@))')
+dat_today["New_Warning"] = ""
 file_date = re.search('\\_(.*)\\.', dat_today_fname).group(1)
 
 ## Alumni email
 mask = dat_today[dat_today["COMPANY EMAIL"].str.contains(r"(^[a-zA-Z]+\.[a-zA-Z]+\.w[a-zA-Z]\d\d@[a-zA-Z]*\.*upenn\.edu$)", na=False)].index
-dat_today.loc[mask, "Flag_Incoming"] = dat_today.loc[mask, "Flag_Incoming"] + f"Alumni-style email. "
+dat_today.loc[mask, "New_Warning"] = dat_today.loc[mask, "New_Warning"] + f"Alumni-style email. "
 
 ## Duplicate name
 mask = dat_today[dat_today.loc[:, ["FIRST NAME", "LAST NAME"]].duplicated(keep = False)].index
-dat_today.loc[mask, "Flag_Incoming"] = dat_today.loc[mask, "Flag_Incoming"] + f"Repeated name in {file_date} file. "
+dat_today.loc[mask, "New_Warning"] = dat_today.loc[mask, "New_Warning"] + f"Repeated name in {file_date} file. "
 
 mask = dat_today[pd.merge(dat_today, dat_ongoing.drop_duplicates(subset=["FIRST NAME", "LAST NAME"]), on=list(["FIRST NAME", "LAST NAME"]), how='left', indicator=True).loc[:, '_merge'] == 'both'].index
-dat_today.loc[mask, "Flag_Incoming"] = dat_today.loc[mask, "Flag_Incoming"] + f"Name from {file_date} exists in old file. "
+dat_today.loc[mask, "New_Warning"] = dat_today.loc[mask, "New_Warning"] + f"Name from {file_date} exists in old file. "
 
 ## Duplicate email
-mask = dat_today[dat_today.loc[:, ["Email_Prefix"]].duplicated(keep = False)].index
-dat_today.loc[mask, "Flag_Incoming"] = dat_today.loc[mask, "Flag_Incoming"] + f"Repeated email prefix in {file_date} file. "
+mask = dat_today[dat_today.loc[:, ["Email_local-part"]].duplicated(keep = False)].index
+dat_today.loc[mask, "New_Warning"] = dat_today.loc[mask, "New_Warning"] + f"Repeated email prefix in {file_date} file. "
 
-mask = dat_today[pd.merge(dat_today, dat_ongoing.drop_duplicates(subset=["Email_Prefix"]), on=list(["Email_Prefix"]), how='left', indicator=True).loc[:, '_merge'] == 'both'].index
-dat_today.loc[mask, "Flag_Incoming"] = dat_today.loc[mask, "Flag_Incoming"] + f"Email prefix from {file_date} exists in old file. "
+mask = dat_today[pd.merge(dat_today, dat_ongoing.drop_duplicates(subset=["Email_local-part"]), on=list(["Email_local-part"]), how='left', indicator=True).loc[:, '_merge'] == 'both'].index
+dat_today.loc[mask, "New_Warning"] = dat_today.loc[mask, "New_Warning"] + f"Email prefix from {file_date} exists in old file. "
 
 # Mark today's records as new
 dat_today["New_Record"] = file_date
@@ -64,17 +64,17 @@ dat_ongoing = pd.read_excel(dat_ongoing_fname, na_values=[], keep_default_na=Fal
 mask_iterate = list(dat_ongoing.index)
 
 ## Follow-up due
-mask = dat_ongoing[dat_ongoing.loc[:, "Followup_Scheduled"] == ""].index
+mask = dat_ongoing[dat_ongoing.loc[:, "Followup_DueDate"] == ""].index
 mask_iterate = list(compress(mask_iterate, [i in set(mask) for i in mask_iterate]))
 
-mask = dat_ongoing[pd.to_datetime(dat_ongoing.loc[:, "Followup_Scheduled"], format='YYYY-MM-DD') <= datetime.today()].index
-dat_ongoing.loc[mask, "New_Action"] = dat_ongoing.loc[mask, "New_Action"] + "Follow up. "
-dat_ongoing.loc[mask, "Followup_Scheduled"] = ""
+mask = dat_ongoing[pd.to_datetime(dat_ongoing.loc[:, "Followup_DueDate"], format='YYYY-MM-DD') <= datetime.today()].index
+dat_ongoing.loc[mask, "Take_Action"] = dat_ongoing.loc[mask, "Take_Action"] + "Follow up. "
+dat_ongoing.loc[mask, "Followup_DueDate"] = ""
 
 ## Alumni account
 
 
 # Merge data to ongoing file
 dat_ongoing["New_Record"] = ""
-dat_ongoing.sort_values(by=["New_Action", "LAST NAME", "FIRST NAME", "COMPANY EMAIL"], inplace=True, ignore_index=True)
+dat_ongoing.sort_values(by=["Take_Action", "LAST NAME", "FIRST NAME", "COMPANY EMAIL"], inplace=True, ignore_index=True)
 dat_ongoing.to_excel(dat_ongoing_fname, sheet_name="in", index=False)
